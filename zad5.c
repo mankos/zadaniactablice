1c74575755df053f48d47a5f7b4d2a2ef08930d2
MarianSkrzypek <tsadowski@sigma.ug.edu.pl>
2014-11-12 16:07:32 +0100

    Update zad5.c

--- zad5.c
+++ zad5.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
-int main(){
-char lancuch[20];
-printf("Podaj lancuch znakow");
-scanf("%s",&lancuch);
+int p=20;
+char string[p];
+int dlugosc(char* string){
+int znaki=0;
 int i;
-for (i=0;i<20;i++){
-printf("%c",lancuch[i]);
+for(i=0;i<20;i++){
+znaki++;
+if(string[i+1]==0){
+break;
 }
+}
+return znaki;
+}
+int main(){
+printf("Podaj znaki\n");
+scanf("%s",string);
+int d;
+d=dlugosc(string);
+while(d!=0){
+d--;
+printf("%c",string[d]);
+if(string[d]==)
+}
+printf("\n");
 return 0;
 }
