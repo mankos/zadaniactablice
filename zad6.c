b67cbea8a5e3d989774ae2ee1183fec526b021a1
MarianSkrzypek <tsadowski@sigma.ug.edu.pl>
2014-11-12 16:07:49 +0100

    Update zad6.c

--- zad6.c
+++ zad6.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
- 
-int palindrom(char* wyraz)
-{
-    char odwrocony[255];
- 
-    //kopiujemy wyraz do zmiennej pomocniczej
-    strcpy (odwrocony, wyraz);
- 
-    //odwracamy wyraz
-    strrev (odwrocony);
- 
-    //jezeli wyrazy sie zgadzaja zwroc 1
-    if ( strcmp(wyraz, odwrocony) == 0)
-        return 1;
- 
-    return 0;
+int main(){
+char lancuch[20];
+printf("Podaj lancuch znakow");
+scanf("%s",&lancuch);
+int i;
+for (i=0;i<20;i++){
+printf("%c",lancuch[i]);
 }
- 
-int main()
-{
-    char wyraz[255];
- 
-    printf ("Podaj wyraz do sprawdzenia:\n");
-    scanf ("%s", wyraz);
- 
-    printf ("%d\n", palindrom(wyraz));
- 
-    system("PAUSE");
-    return 0;
+return 0;
 }
