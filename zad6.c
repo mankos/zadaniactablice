e5bf3f0faa54530fb060752b9346b8a61d90b411
MarianSkrzypek <tsadowski@sigma.ug.edu.pl>
2014-11-12 15:54:25 +0100

    Update zad6.c

---
+++ zad6.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int pali(char* wyraz)
+{
+int d;
+char odwrocony[20];
+
+strcpy (odwrocony, wyraz);
+strrev (odwrocony);
+d=strrev (odwrocony);
+
+    if ( d == 0)
+        return 1;
+
+    return 0;
+}
+
+int main()
+{
+  char wyraz[20];
+printf ("Podaj string\n");
+scanf ("%s", wyraz);
+printf ("%d\n", d);
+system("PAUSE");
+    return 0;
+}
